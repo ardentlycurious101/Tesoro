94052c33045f4b915af254dc618aa674ef39a534
Elina <ardentlycurious101@gmail.com>
2019-08-01 13:35:15 -0700

    Final app.

---
+++ Tesoro/CustomMessageCell.swift
@@ -0,0 +1,28 @@
+//
+//  CustomMessageCell.swift
+//  Tesoro
+//
+//  Created by Elina Lua Ming on 7/14/19.
+//  Copyright © 2019 Elina Lua Ming. All rights reserved.
+//
+
+import UIKit
+
+class CustomMessageCell: UITableViewCell {
+
+
+    @IBOutlet var messageBackground: UIView!
+    @IBOutlet var avatarImageView: UIImageView!
+    @IBOutlet var messageBody: UILabel!
+    @IBOutlet var senderUsername: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code goes here
+        
+        
+        
+    }
+
+
+}
